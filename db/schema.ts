f429183e5493876e9148451852030e3ee8f3c75e
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-04-14 23:19:47 -0700

    [scripts] add: database seeding script

--- db/schema.ts
+++ db/schema.ts
@@ -4,7 +4,6 @@ import { tsvector } from "./tsvector";
 export const champions = pgTable("champions", {
     id: serial("id").primaryKey(),
     name: text("name").notNull(),
-    description: text("biography").notNull(),
     releaseDate: timestamp("release_date").notNull(),
     vector: tsvector("vector", { sources: ["name", "description"] }),
 });
---
+++ src/lib/dataLoader.js
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// @ts-expect-error - JS require for a script
+const { parse } = require("node-html-parser");
+// @ts-expect-error - JS require for a script
+const { existsSync, readFileSync, writeFileSync } = require("fs");
+// @ts-expect-error - JS require for a script
+const { createClient } = require("redis");
+const { Client } = require("pg");
+
+const tableFilePath = "../../champs-table.html";
+const redisClientPromise = createClient({
+    url: process.env.REDIS_URL,
+})
+    .on("error", (err) => console.error(err))
+    .connect();
+const pgClient = new Client({
+    connectionString: process.env.PG_URL,
+});
+
+async function getTableData() {
+    // dont repeatedly crawl the table
+    if (existsSync(tableFilePath)) {
+        console.log("Local file exists, reading it instead of fetching.");
+        const buf = readFileSync(tableFilePath);
+        return buf.toString();
+    } else {
+        const response = await fetch(
+            "https://leagueoflegends.fandom.com/wiki/List_of_champions",
+        );
+        const data = await response.text();
+        writeFileSync(tableFilePath, data);
+        return data;
+    }
+}
+
+async function pullChampsData() {
+    const pageHtml = await getTableData();
+    const pageRoot = parse(pageHtml);
+    const tableRoot = pageRoot.querySelector("table.article-table");
+    if (!tableRoot) {
+        throw new Error("Loaded page content is of unexpected form.");
+    }
+    const tableBody = tableRoot.querySelector("tbody");
+    const champions = [];
+    for (const row of tableBody.getElementsByTagName("tr").slice(1)) {
+        const rowRawText = row.rawText;
+        const [nameTitle, , releaseString] = rowRawText.split("\n\n");
+        const [, name] = nameTitle.split("\n");
+        champions.push({ name, releaseDate: releaseString });
+    }
+    return champions;
+}
+
+async function storeDataRedis(data) {
+    const redisClient = await redisClientPromise;
+    for (const champion of data) {
+        const { name } = champion;
+        const champTerms = [];
+        for (let i = 0; i < name.length; i++) {
+            champTerms.push({ score: 0, value: name.substring(0, i) });
+        }
+        champTerms.push({ score: 0, value: name + "*" });
+        const populateDb = async () => {
+            await redisClient.zAdd("champTerms", champTerms);
+        };
+
+        populateDb();
+    }
+}
+async function storeDataPg(data) {
+    await pgClient.connect();
+    await pgClient.query(
+        "CREATE TABLE IF NOT EXISTS champions(id SERIAL PRIMARY KEY, name TEXT NOT NULL, release_date DATE NOT NULL, vector TSVECTOR NOT NULL);",
+    );
+    for (const champion of data) {
+        const { name, releaseDate } = champion;
+        await pgClient.query(
+            "INSERT INTO champions(name, release_date, vector) \
+             VALUES ($1::text, $2::date, setweight(to_tsvector($1::text), 'A') || setweight(to_tsvector($2::text), 'B'));",
+            [name, releaseDate],
+        );
+    }
+}
+async function storeData(data) {
+    storeDataPg(data);
+    storeDataRedis(data);
+}
+
+async function main() {
+    const champArray = await pullChampsData();
+    await storeData(champArray);
+}
+main().then(() => {
+    console.log("finished with main");
+});
