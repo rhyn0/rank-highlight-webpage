47d2da06221318b090ec2bd1010ac328f5cdd0ae
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-04-16 01:40:41 -0700

    add: some simple styling components

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+import { ThemeToggle } from "./themeToggle";
+import { cn } from "@/lib/utils";
+
+interface HeaderProps {
+    className?: string;
+}
+
+export function Header({ className }: HeaderProps) {
+    return (
+        <header className={cn("bg-secondary", className)}>
+            <div className="container flex">
+                <div>
+                    <Link
+                        href="/"
+                        scroll
+                    >
+                        Title
+                    </Link>
+                </div>
+                <div className="flex flex-1 md:justify-end">
+                    <ThemeToggle />
+                </div>
+            </div>
+        </header>
+    );
+}
